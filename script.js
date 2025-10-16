6a779d4f27ee4fa799756c4356042b8d1b983dd4
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-16 14:30:17 -0400

    added more variables, game loops, listeners and comments

--- script.js
+++ script.js
@@ -1,10 +1,66 @@
+// variables
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d')
 
 let speed = 7;
 
-// game function
-function drawGame() {
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
 
+let xVelocity=0;
+let yVelocity=0;
+
+// game loop
+function drawGame() {
+    clearScreen();
+    changeSnakePosition();
+    drawSnake();
     setTimeout(drawGame, 1000/speed);
 }
+
+function clearScreen() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,canvas.clientWidth,canvas.height);
+}
+
+function drawSnake() {
+    ctx.fillStyle = 'white'
+    ctx.fillRect(headX * tileCount, headY * tileCount, tileSize,tileSize)
+}
+
+function changeSnakePosition() {
+    headX = headX + xVelocity;
+    headY = headY + yVelocity;
+}
+
+// keyboard listeners
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event) {
+    //up
+    if(event.keyCode == 38){
+        yVelocity = -1;
+        xVelocity = 0;
+    }
+
+    //down
+    if(event.keyCode == 40){
+        yVelocity = 1;
+        xVelocity = 0;
+    }
+
+    //left
+    if(event.keyCode == 37){
+        yVelocity = 0;
+        xVelocity = -1;
+    }
+
+    //right
+    if(event.keyCode == 39){
+        yVelocity = 0;
+        xVelocity = 1;
+    }
+}
+drawGame();
