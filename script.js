6bf8ff7fc0aad0c37ba0746af84186b5a65eb069
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-16 14:39:58 -0400

    added a green background so it looks like the snake is on grass and added code where the snake can move in any direction by the user

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function drawGame() {
 }
 
 function clearScreen() {
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'green';
     ctx.fillRect(0,0,canvas.clientWidth,canvas.height);
 }
 
@@ -41,24 +41,32 @@ document.body.addEventListener('keydown', keyDown);
 function keyDown(event) {
     //up
     if(event.keyCode == 38){
+        if(yVelocity == 1)
+            return;
         yVelocity = -1;
         xVelocity = 0;
     }
 
     //down
     if(event.keyCode == 40){
+        if(yVelocity == -1)
+            return;
         yVelocity = 1;
         xVelocity = 0;
     }
 
     //left
     if(event.keyCode == 37){
+        if(xVelocity == 1)
+            return;
         yVelocity = 0;
         xVelocity = -1;
     }
 
     //right
     if(event.keyCode == 39){
+        if(xVelocity == -1)
+            return;
         yVelocity = 0;
         xVelocity = 1;
     }
